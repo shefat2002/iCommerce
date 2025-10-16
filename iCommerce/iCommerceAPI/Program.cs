a2286e7202de11ba22b07e6f1174bc286c75bf60
shefat2002 <shefat2002@gmail.com>
2025-10-17 03:31:08 +0600

    added swaggar to API Project

--- iCommerce/iCommerceAPI/Program.cs
+++ iCommerce/iCommerceAPI/Program.cs
@@ -1,15 +1,40 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.OutputFormatters.RemoveType<SystemTextJsonOutputFormatter>();
+    options.OutputFormatters.Add(new SystemTextJsonOutputFormatter(new JsonSerializerOptions
+    {
+        ReferenceHandler = ReferenceHandler.IgnoreCycles,
+        PropertyNameCaseInsensitive = false,
+        PropertyNamingPolicy = null,
+        WriteIndented = true,
+        TypeInfoResolver = JsonSerializerOptions.Default.TypeInfoResolver
+    }));
+});
+
+//Swagger
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
